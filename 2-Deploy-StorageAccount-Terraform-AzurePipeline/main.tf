7e215db6ebac6f2b2e95fbaaccfbecb161223288
acmarpu <acmarpu@gmail.com>
2025-05-25 15:35:57 +0530

    backend details

--- 2-Deploy-StorageAccount-Terraform-AzurePipeline/main.tf
+++ 2-Deploy-StorageAccount-Terraform-AzurePipeline/main.tf
@@ -1,29 +1,27 @@
-# terraform required_providers setion is optional here
 terraform {
   required_providers {
     azurerm = {
-        sosource = "hashicorp/azurerm"
-        versioversion = "=2.46.0"
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
     }
   }
 }
 
 provider "azurerm" {
-    version = "=2.13.0"
-    features {
-      
-    }
-  
+  features {}
 }
+
+data "azurerm_client_config" "current" {}
+
 # backend azure storage account details 
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "StorageAccount-ResourceGroup"
-    storage_account_name = "abcd1234"
-    container_name       = "tfstate"
-    key                  = "prod.terraform.tfstate"
-  }
-}
+# terraform {
+#   backend "azurerm" {
+#     resource_group_name  = "StorageAccount-ResourceGroup"
+#     storage_account_name = "abcd1234"
+#     container_name       = "tfstate"
+#     key                  = "prod.terraform.tfstate"
+#   }
+# }
 
 #Use this data source to access the configuration of the Azure Resource Manager provider.
 data "azurerm_client_config" "current" {}
