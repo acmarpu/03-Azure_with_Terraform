0b50021c5dbfc97941cdafeffe6980b1b364f3d6
acmarpu <acmarpu@gmail.com>
2025-05-25 16:01:37 +0530

    Updated25052025:16:01

--- 2-Deploy-StorageAccount-Terraform-AzurePipeline/main.tf
+++ 2-Deploy-StorageAccount-Terraform-AzurePipeline/main.tf
@@ -30,15 +30,15 @@ provider "azurerm" {
 # below code for azure storage account details
 resource "azurerm_resource_group" "example" {
   name     = "example-resources"
-  location = "West Europe"
+  location = "eastus2"
 }
 
 resource "azurerm_storage_account" "example" {
-  name                     = "storageaccountname"
+  name                     = "stg2jai"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
-  account_replication_type = "GRS"
+  account_replication_type = "LRS"
 
   tags = {
     environment = "staging"
